450400e0cd598b692595f46f746cc1e2a9f81062
VictorLima <victoraugustolima1@gmail.com>
2023-02-16 18:45:12 -0300

    projeto criado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace supermercadoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
