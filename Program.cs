1407b7788e8f50be1497e21f1d6df8c5819ff1a1
VictorLima <victoraugustolima1@gmail.com>
2023-02-17 11:22:24 -0300

    banco de dados (fixes#01)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using supermercadoMVC.context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// registrar banco de dados
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
